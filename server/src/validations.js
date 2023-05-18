93588aa60d6b8f54b7b5ebc9f484b49c6a442f76
ngyida <e0726193@u.nus.edu>
2023-05-19 00:18:17 +0800

    escape special characters in backend

--- server/src/validations.js
+++ server/src/validations.js
@@ -3,10 +3,12 @@ const { check: body } = require('express-validator');
 exports.tutorialInputValidation = [
     body('title')
     .notEmpty()
-    .isLength({ max: 100 }),
+    .isLength({ max: 100 })
+    .escape(),
     body('description')
     .notEmpty()
-    .isLength({ max: 100 }),
+    .isLength({ max: 100 })
+    .escape(),
     body('published')
     .notEmpty()
     .isBoolean()
