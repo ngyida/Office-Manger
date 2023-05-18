56ec4622496b30fb2354f0093496f21353602f20
ngyida <e0726193@u.nus.edu>
2023-05-18 21:57:48 +0800

    adjust server-side input validation to match client side's

--- server/src/validations.js
+++ server/src/validations.js
@@ -3,21 +3,11 @@ const { check: body } = require('express-validator');
 exports.tutorialInputValidation = [
     body('title')
     .notEmpty()
-    .isLength({ max: 10000 }),
+    .isLength({ max: 100 }),
     body('description')
     .notEmpty()
-    .isLength({ max: 10000 }),
+    .isLength({ max: 100 }),
     body('published')
     .notEmpty()
     .isBoolean()
 ];
-
-// exports.tutorialUpdateValidation = [
-//     body().custom((value, { req }) => {
-//       const { title, description, published } = req.body;
-//       if (!title && !description && !published) {
-//         return false;
-//       }
-//       return true;
-//     }),
-//   ];
